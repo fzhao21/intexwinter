8ec02f4609e5f0f3a882e28b0b7e3f17f5431bb6
Ryan Jackson <rmjackson234@gmail.com>
2023-04-13 21:28:00 -0600

    Map IFrame

--- src/Pages/UnsupervisedAnalysis.jsx
+++ src/Pages/UnsupervisedAnalysis.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const UnsupervisedAnalysis = props => {
-    return(
-        <>
-            <div>
-                <p>Unsupervised Analysis</p>
-            </div>
-        </>
-    )
-}
+const UnsupervisedAnalysis = (props) => {
+  return (
+    <>
+      <iframe src="./arcGIS.html" style={{ width: 1500, height: 700 }}></iframe>
+      <div
+        id="viewDiv"
+        style={{ padding: 0, margin: 0, height: "100%", width: "100%" }}
+      ></div>
+    </>
+  );
+};
 
-export default UnsupervisedAnalysis
+export default UnsupervisedAnalysis;
