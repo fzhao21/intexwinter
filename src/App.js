8d919cd24f509e220d4514876e91f0a37e8e92d7
fzhao21 <falinezhao21@gmail.com>
2023-04-14 00:55:26 -0600

    plsss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< ours
 import "./App.css";
 import logo from "./logo.svg";
 import Home from "./Pages/Home";
@@ -20,6 +21,19 @@ import "@aws-amplify/ui-react/styles.css";
 import { Auth, Amplify } from "aws-amplify";
 
 function App({ signOut, user }) {
+=======
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+import NavBar from './components/Navbar';
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Summary from './Pages/Summary';
+import Login from './Pages/Login'
+import Logout from './Pages/Logout'
+
+const App = () => {
+>>>>>>> theirs
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLogin = () => {
@@ -29,6 +43,7 @@ function App({ signOut, user }) {
   const handleLogout = () => {
     setIsAuthenticated(false);
   };
+<<<<<<< ours
 
   // let groups = user.signInUserSession.accessToken.payload["cognito:groups"];
   // groups = groups[0];
@@ -67,7 +82,37 @@ function App({ signOut, user }) {
         <CookieFooter />
         <Footer/>
       </BrowserRouter>
+=======
+
+  return (
+    <div>
+      <NavBar isAuthenticated={isAuthenticated} />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/about" component={About} /> */}
+        <Route path="/login">
+          <Login onLogin={handleLogin} />
+        </Route>
+        {/* <Route path="/register" component={Register} /> */}
+        <Route path="/dashboard">
+          {isAuthenticated ? (
+            <Summary onLogout={handleLogout} />
+          ) : (
+            <Summary to="/login" />
+          )}
+        </Route>
+        <Route path="/logout">
+          {isAuthenticated ? (
+            <Logout onLogout={handleLogout} />
+          ) : (
+            <Navigate to="/" />
+          )}
+        </Route>
+      </Routes>
+      <Home />
+    </div>
+>>>>>>> theirs
   );
-}
+};
 
 export default App;
